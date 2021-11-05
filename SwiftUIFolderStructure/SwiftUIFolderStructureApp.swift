e89ae7e91b06756e11d2ee390e852f83a421995a
pooya <info@centurytrail.com>
2021-11-05 03:01:17 -0700

    Initial Commit

---
+++ SwiftUIFolderStructure/SwiftUIFolderStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFolderStructureApp.swift
+//  SwiftUIFolderStructure
+//
+//  Created by Pooya on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFolderStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
